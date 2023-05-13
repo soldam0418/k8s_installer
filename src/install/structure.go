ae2a21e8193c157d592238c03c2a3671a767bf5e
jaehopark <107022839+hojun121@users.noreply.github.com>
2023-05-13 21:33:23 +0900

    Modify StrictHostKeyChecking cmd

--- src/install/structure.go
+++ src/install/structure.go
@@ -26,14 +26,14 @@ func (hs *HandlerStruct) getConfig(cfg *src.Config, configDir string) (err error
 	hs.AllNodes = append(hs.Cfg.Masters, hs.Cfg.Workers...)
 	// Get Base Path. ex) baseDir := "/home1/irteamsu"
 	log.Println("Get Instance Base Path")
-	hs.HostBaseDir = strings.ReplaceAll(src.SshCMDToGetOutput(fmt.Sprintf("sshpass -o StrictHostKeyChecking=no ssh %s@%s pwd", hs.Cfg.User, hs.Cfg.Masters[0])), "\n", "")
+	hs.HostBaseDir = strings.ReplaceAll(src.SshCMDToGetOutput(fmt.Sprintf("ssh -o StrictHostKeyChecking=no %s@%s pwd", hs.Cfg.User, hs.Cfg.Masters[0])), "\n", "")
 	return nil
 }
 
 func (hs *HandlerStruct) SCPK8sSetup(wg *sync.WaitGroup) (isOk bool) {
 	stepIsOK := true
 	src.SshCMDToAllNodesByChannel(wg, hs.AllNodes,
-		fmt.Sprintf("sshpass -o StrictHostKeyChecking=no scp %s %s", fmt.Sprintf("%s/k8s_setup.sh", hs.CfgDir),
+		fmt.Sprintf("scp -o StrictHostKeyChecking=no %s %s", fmt.Sprintf("%s/k8s_setup.sh", hs.CfgDir),
 			fmt.Sprintf("%s@nodeip:%s", hs.Cfg.User, hs.HostBaseDir)), &stepIsOK)
 	if !stepIsOK {
 		return false
@@ -44,7 +44,7 @@ func (hs *HandlerStruct) SCPK8sSetup(wg *sync.WaitGroup) (isOk bool) {
 func (hs *HandlerStruct) ExecuteK8sSetup(wg *sync.WaitGroup) (isOk bool) {
 	stepIsOK := true
 	src.SshCMDToAllNodesByChannel(wg, hs.AllNodes,
-		fmt.Sprintf("sshpass -o StrictHostKeyChecking=no ssh %s sh %s", fmt.Sprintf("%s@nodeip", hs.Cfg.User),
+		fmt.Sprintf("ssh -o StrictHostKeyChecking=no %s sh %s", fmt.Sprintf("%s@nodeip", hs.Cfg.User),
 			fmt.Sprintf("%s/%s", hs.HostBaseDir, "k8s_setup.sh")), &stepIsOK)
 	if !stepIsOK {
 		return false
@@ -53,7 +53,7 @@ func (hs *HandlerStruct) ExecuteK8sSetup(wg *sync.WaitGroup) (isOk bool) {
 }
 
 func (hs *HandlerStruct) KubeadmInit() (kdmMasterJoinCMD string, kdmWorkerJoinCMD string) {
-	kdmJoinCMD := fmt.Sprintf("sshpass -o StrictHostKeyChecking=no ssh %s sudo kubeadm init --kubernetes-version %s --pod-network-cidr %s --upload-certs",
+	kdmJoinCMD := fmt.Sprintf("ssh -o StrictHostKeyChecking=no %s sudo kubeadm init --kubernetes-version %s --pod-network-cidr %s --upload-certs",
 		fmt.Sprintf("%s@%s", hs.Cfg.User, hs.Cfg.Masters[0]), hs.Cfg.K8sVersion, hs.Cfg.PodNetworkCidr)
 	if hs.Cfg.ControlPlaneEndpoint != "" {
 		kdmJoinCMD = kdmJoinCMD + " " + fmt.Sprintf("--control-plane-endpoint %s", hs.Cfg.ControlPlaneEndpoint)
@@ -64,7 +64,7 @@ func (hs *HandlerStruct) KubeadmInit() (kdmMasterJoinCMD string, kdmWorkerJoinCM
 
 func (hs *HandlerStruct) KubeadmJoin(wg *sync.WaitGroup, nodes []string, kdmJoinCMD string) bool {
 	stepIsOK := true
-	src.SshCMDToAllNodesByChannel(wg, nodes, fmt.Sprintf("sshpass -o StrictHostKeyChecking=no ssh %s sudo %s",
+	src.SshCMDToAllNodesByChannel(wg, nodes, fmt.Sprintf("ssh -o StrictHostKeyChecking=no %s sudo %s",
 		fmt.Sprintf("%s@nodeip", hs.Cfg.User), kdmJoinCMD), &stepIsOK)
 	return stepIsOK
 }
@@ -73,13 +73,13 @@ func (hs *HandlerStruct) ExecuteBashScript() {
 	deployDir := hs.CfgDir + "/deploy"
 	files, _ := ioutil.ReadDir(deployDir)
 	filesCnt := len(files)
-	err := exec.Command("sshpass", "-o", "StrictHostKeyChecking=no", "scp", "-r", deployDir,
+	err := exec.Command("scp", "-o", "StrictHostKeyChecking=no", "-r", deployDir,
 		fmt.Sprintf("%s@%s:%s", hs.Cfg.User, hs.Cfg.Masters[0], hs.HostBaseDir)).Run()
 	if err != nil {
 		log.Println("fail to scp config directory. err: ", err.Error())
 	}
 	for i := 0; i < filesCnt; i++ {
-		err := exec.Command("sshpass", "-o", "StrictHostKeyChecking=no", "ssh", fmt.Sprintf("%s@%s", hs.Cfg.User, hs.Cfg.Masters[0]),
+		err := exec.Command("ssh", "-o", "StrictHostKeyChecking=no", fmt.Sprintf("%s@%s", hs.Cfg.User, hs.Cfg.Masters[0]),
 			"sh", fmt.Sprintf("%s/%s/%d*", hs.HostBaseDir, "deploy", i+1)).Run()
 		if err != nil {
 			log.Println(fmt.Sprintf("[ %s ] excute fail. ", files[i].Name()), "err: ", err.Error())
