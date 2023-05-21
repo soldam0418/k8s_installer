95d2de11c0ed8a707b66967dc873bbadea5a3799
jaehopark <jaehopark@nhn.com>
2023-05-21 20:18:11 +0900

    Update k8s remove script

--- config/k8s_remove.sh
+++ config/k8s_remove.sh
@@ -1,13 +1,13 @@
 #/bin/bash
-sudo swapoff -a
-sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl -y
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt install kubeadm=1.21.0-00 kubelet=1.21.0-00 kubectl=1.21.0-00 containerd=1.3.3-0ubuntu2 docker.io=19.03.8-0ubuntu1 -y
-sudo apt-mark hold kubeadm kubelet kubectl docker
-sudo systemctl restart docker
-sudo systemctl restart kubelet
-sudo systemctl enable docker.service
+sudo kubeadm reset --force
+sudo systemctl stop kubelet
+sudo systemctl stop docker
+sudo apt-get purge kubeadm kubelet kubectl docker --auto-remove -y
+sudo rm -rf /var/lib/cni/
+sudo rm -rf /var/lib/kubelet/*
+sudo rm -rf /etc/cni/
+sudo rm -rf /etc/kubernetes
+sudo rm -rf $HOME/.kube
+sudo ifconfig cni0 down
+sudo ifconfig flannel.1 down
+sudo ifconfig docker0 down
