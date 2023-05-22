726ec295fae61b2a7b9937b32aecc14b5caf65c9
irteam su-account <irteamsu@bastion-0o7k2b6a.svr.toastmaker.net>
2023-05-22 09:35:44 +0900

    Update remove script

--- config/k8s_remove.sh
+++ config/k8s_remove.sh
@@ -2,11 +2,13 @@
 sudo kubeadm reset --force
 sudo systemctl stop kubelet
 sudo systemctl stop docker
-sudo apt-get purge kubeadm kubelet kubectl docker --auto-remove -y
+sudo apt-mark unhold kubeadm kubelet kubectl docker
+sudo apt-get purge kubeadm kubelet kubectl docker.io --auto-remove -y
 sudo rm -rf /var/lib/cni/
 sudo rm -rf /var/lib/kubelet/*
 sudo rm -rf /etc/cni/
 sudo rm -rf /etc/kubernetes
+sudo rm -rf /etc/docker
 sudo rm -rf $HOME/.kube
 sudo ifconfig cni0 down
 sudo ifconfig flannel.1 down
