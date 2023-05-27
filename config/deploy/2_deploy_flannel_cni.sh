82fdc599252404e4e38e65551851dc19b0f2c873
jaehopark <107022839+hojun121@users.noreply.github.com>
2023-05-27 12:45:36 +0900

    Update 2_deploy_flannel_cni.sh

--- config/deploy/2_deploy_flannel_cni.sh
+++ config/deploy/2_deploy_flannel_cni.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
